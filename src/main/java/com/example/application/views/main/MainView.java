41b7eb00bef24c9fdb82245f22392a42cb0395b5
lsmarsden <lsmarsden@hotmail.co.uk>
2022-05-10 21:51:36 +0100

    VN-2 Create Template TodoList component

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -4,8 +4,6 @@ import com.example.application.views.task.TodoList;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import lombok.AccessLevel;
-import lombok.Getter;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,7 +12,6 @@ public class MainView extends VerticalLayout implements InitializingBean {
 
     private final H1 title = new H1("Vaadin Todo");
 
-    @Getter(AccessLevel.PACKAGE)
     @Autowired
     private TodoList todoList;
 
